d63c8505b8b3de3e920eca626fb59c8de8e306cb
Albina Krasykova <91033995+AlbinaKrasykova@users.noreply.github.com>
2023-06-26 13:03:26 -0400

    Update Score_arg.py
    First version that actually works

--- Score_arg.py
+++ Score_arg.py
@@ -3,16 +3,27 @@ import argparse
 import cv2 
 from PIL import Image
 import numpy as np
+#import show_2img_function.py
+
+#GOAL: score function which scores ing pppln according to how well it perfomed 
+
+#Result: does run, but doesn't detect images at all
+
+#FGOAL: takes both 2dprinted tags(test) and 3d printed tags and detects the id's correctl, scores it and return the ratio 
+
+# NEEDS TO BE DONE : check the datatype whch is return by the function, rewrite all the fucntion manually 
 
 dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
 parameters = cv2.aruco.DetectorParameters 
 detector = cv2.aruco.ArucoDetector(dictionary)
 
-#0. Function that Generates dictionary with images from the file 
-# Key: 9_angle_6_.png, Value: <PIL.PngImagePlugin.PngImageFile image mode=RGB size=1920x1080 at 0x178C0FF6BF0>
+
 
 import os
 from PIL import Image
+
+#0. Function that Generates dictionary with images from the file 
+# Key: 9_angle_6_.png, Value: <PIL.PngImagePlugin.PngImageFile image mode=RGB size=1920x1080 at 0x178C0FF6BF0>
 
 def load_images(directory):
     image_dict = {}
@@ -26,17 +37,29 @@ def load_images(directory):
                 print(f"Unable to open image: {filename}")
     return image_dict
 
-image_directory = "D:\AI research internship\opencv_scripts\data_set"
+# p_dataset
+# data_set
+
+image_directory = "D:\AI research internship\opencv_scripts\p_dataset"
 
 
 
 #2 CREATE an ARRAY with Tag original ID's by parcing the key string, as getting first digits of it and save to an array 'Original ID' - Key: 9_angle_6_.png - > 9
+
+#check type 
+
 def original_id(image_dict):
     digit_array = []
     for key in image_dict.keys():
         first_digit = next((char for char in key if char.isdigit()), None)
         if first_digit:
-            digit_array.append(int(first_digit))
+            #convert from str to int 
+            first_digit_int = int(first_digit)
+            digit_array.append(int(first_digit_int))
+            #check datatype 
+            print(type(first_digit_int))
+            
+
     return digit_array
 
 
@@ -55,12 +78,17 @@ def predicted_id2_c(image_dict):
                                     cv2.THRESH_BINARY_INV, 51, 1)
         blurred = cv2.GaussianBlur(th3, (21, 21), 0)
         flipped = cv2.flip(blurred, 1)
-        _, ids, _ = detector.detectMarkers(flipped)
+        corners, ids, rejected = detector.detectMarkers(flipped)
         p_id_arr.append(ids)
+        print(type(ids))
+    print(p_id_arr)
     return p_id_arr
 
 
 #print(p_id_arr = predicted_id(image_dict))
+
+
+#4 Function that combines and displays 2 images side by side  
 
 def combined_2(img1, img2):
 
@@ -79,6 +107,7 @@ def combined_2(img1, img2):
  return combined_image
 
 
+#5 Scoring Function 
 
 def score(original_id, predicted_id):
   scores = 0
@@ -106,9 +135,11 @@ def score_c(original_id, predicted_id):
             scores += 1
 
  if scores != 0:
-    ratio = img_count / scores
+    ratio =  scores/img_count
 
  return img_count, scores, ratio
+
+#6 Display ratio based on the precious scoring subfunction  
 
 
 def info(img_count,score,ratio):
@@ -117,6 +148,7 @@ def info(img_count,score,ratio):
      ratio = ratio
      print(f"Image processing pipeline scored at {score}")
      print(f"Out of {img_count} images {score} were predicted")
+     print('Score:', int(ratio*100), '%')
 
      
 
@@ -161,9 +193,6 @@ if args.score:
 
   predicted_ids = predicted_id2_c(image_dict)
 
- #Code Check predicted_ids - works 
-
-  print(predicted_ids)
 
   img_count,scores,ratio = score_c(original_ids,predicted_ids)
 
@@ -189,7 +218,7 @@ elif args.score_with_images:
    info(img_count,scores,ratio)
  
    while(True):
-     ret, frame = cap.read()
+    #  ret, frame = cap.read()
      for key, values in image_dict.items():
          img1 = cv2.imread(key)
          img2 = processed(img1)
@@ -200,6 +229,7 @@ elif args.score_with_images:
     
      
      if cv2.waitKey(1) & 0xFF == ord('x'):
+         combined.close()
          break
      
      
