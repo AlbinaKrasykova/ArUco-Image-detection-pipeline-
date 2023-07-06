81ce634a1651250388399416a0dca55d723e118b
Albina Krasykova <91033995+AlbinaKrasykova@users.noreply.github.com>
2023-07-06 19:10:16 -0400

    Add files via upload
    Updated

--- Score_arg.py
+++ Score_arg.py
@@ -5,33 +5,10 @@ from PIL import Image
 import numpy as np
 from shadow_highlight_correction import correction
 import math
-
-calibration_frame = None
+import os
 
 #I was trying to import functions but I got work more of functions I importing like these which imports images 
 #from Amrits_ppln import correction , contrast, threshold, calibration_frame, clahe, blur, THRESHOLD_PX_VAL
-
-
-
-''''
-Image processing pipeline scored at 180 %
-Out of 41 images 74 were predicted
-Score: 180.4878048780488 %
-#FIXED Monday 7/3 - changed scoring function 
-#changed tabs order 
-
-Total images 41
-Predicted ing: 41
-29
-
-Printing Amrits ppln score
-
-Image processing pipeline scored at 70 %
-Out of 41 images 29 were predicted
-Score: 70.73170731707317 %
-'''
-
-
 
 #from Fahads_ppln import Fdetection, Ftransformation  - for some rrn I got the img wth an id as well, so its imports everything I have ✔
 #from save_frame  import detect_F, process_frame_F ✔
@@ -53,24 +30,17 @@ Score: 70.73170731707317 %
 
 #7: Dataset from different angles/calculating the distance - ✔
 
-#  image processing in batches, implement and rewrite functions 
+#  image processing in batches, implement and rewrite functions - ✔
 
-# NEEDS TO BE DONE 7: Confusion matrix implnt (precision and recall)
-
-
+# NEEDS TO BE DONE 7: Confusion matrix implnt (precision and recall) - ✔
 
 
-#ERRORS: 
 
 
 dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
 parameters = cv2.aruco.DetectorParameters 
 detector = cv2.aruco.ArucoDetector(dictionary)
 
-
-
-import os
-from PIL import Image
 
 #0. Function that Generates dictionary with images from the file - ✔
 # Key: 9_angle_6_.png, Value: <PIL.PngImagePlugin.PngImageFile image mode=RGB size=1920x1080 at 0x178C0FF6BF0>
@@ -151,8 +121,6 @@ def load_images_in_batches(directory, batch_size, batch_index=0):
 
 
 
-# p_dataset
-# data_set
 
 
 
@@ -165,21 +133,7 @@ def load_images_in_batches(directory, batch_size, batch_index=0):
 #Info: Original id's of the images which is an array of ints  
 
 #Requirments: image_dict, returns an array of ints,
-''''
-def original_id(image_dict):
-    digit_array = []
-    for key in image_dict.keys():
-        first_digit = next((char for char in key if char.isdigit()), None)
-        if first_digit:
-            #convert from str to int 
-            first_digit_int = int(first_digit)
-            digit_array.append(int(first_digit_int))
-            #check datatype 
-            print(type(first_digit_int))
-            
 
-    return digit_array
-    '''
 
 
 import re
@@ -215,8 +169,6 @@ def My_predicted_id(image_dict):
         _, ids, _ = detector.detectMarkers(flipped)
         p_id_arr.append(ids)
 
-    
-        #print(type(ids))
     print('My array with predicted id is: ', p_id_arr)
     return p_id_arr
 
@@ -239,30 +191,22 @@ def F_predicted_id2(image_dict):
     print('Fahads array with the predicted id is: ', p_id_arr)
     return p_id_arr
 
-# A_ppln CREATE an ARRAY with Tags that were predicted - 
+# A_ppln CREATE an ARRAY with Tags that were predicted 
+calibration_frame = None
 
-#correction
+THRESHOLD_PX_VAL = 100
 
-#calibration_frame
-
-#contrast
+CLIP_LIMIT = 20.0 
 
 def contrast(image, clahe):
     clahe_out = clahe.apply(image)
     return clahe_out
 
-
-#blur
-
 def blur(image, kernel):
     return cv2.blur(image, kernel)
 
-#threshold
-
-
 def threshold(image, px_val):
-    # ret, thresholded = cv2.threshold(image, px_val, 255, cv2.THRESH_TOZERO)
-    # ret, thresholded = cv2.threshold(thresholded, px_val, 1, cv2.THRESH_TOZERO_INV)
+
     thresholded = cv2.adaptiveThreshold(image, 
                                         255, 
                                         cv2.ADAPTIVE_THRESH_MEAN_C, 
@@ -270,12 +214,6 @@ def threshold(image, px_val):
                                         89, 
                                         2)
     return thresholded
-
-#THRESHOLD_PX_VAL 
-
-THRESHOLD_PX_VAL = 100
-
-CLIP_LIMIT = 20.0 
 
 
 def drawMarkers(img, corners, ids, borderColor=(255,0,0), thickness=25):
@@ -333,26 +271,11 @@ def A_predicted_id2(image_dict):
         img_contrast_enhanced = contrast(img_norm, clahe)
         img_blurred = blur(img_contrast_enhanced, (5, 5))
         img_thresholded = threshold(img_blurred, THRESHOLD_PX_VAL)
-        # with no flip  - I got 9% score
-        # with flip - I got 180%, 41 out of 74
         flipped = cv2.flip(img_thresholded, 1)
         ids = A_detect(flipped)
         p_id_arr.append(ids)
-        #print(type(ids))
-        #print(ids)
     print('Amrits array with predicted id is: ', p_id_arr)
     return p_id_arr
-
-
-
-
-
-
-
-
-
-
-#print(p_id_arr = predicted_id(image_dict))
 
 
 #4 Function that combines and displays 2 images side by side  - ✔
@@ -373,8 +296,7 @@ def combined_2(img1, img2):
 
  return combined_image
 
-
-#5 Scoring Function - ✔ +precision and recall 
+# Calculates the precison and recall - ✔ 
 
 def calc_p_r(original_ids, predicted_ids):
     true_positive = 0
@@ -389,11 +311,16 @@ def calc_p_r(original_ids, predicted_ids):
 
     false_negative = len(original_ids) - true_positive
 
-    precision = true_positive / (true_positive + false_positive)
-    recall = true_positive / (true_positive + false_negative)
+    precision = 0
+    recall = 0
+
+    if true_positive + false_positive != 0:
+        precision = true_positive / (true_positive + false_positive)
+    
+    if true_positive + false_negative != 0:
+        recall = true_positive / (true_positive + false_negative)
 
     return precision, recall
-
 
 
 def score(original_id, predicted_id):
@@ -405,14 +332,7 @@ def score(original_id, predicted_id):
     print('Predicted images:', predicted_id_count)
 
     for id in original_id:
-        if isinstance(
-            
-            
-            
-            
-            
-            
-            id, int) and id in predicted_id:
+        if isinstance(id, int) and id in predicted_id:
             scores += 1
 
     precision, recall = calc_p_r(original_id, predicted_id)
@@ -456,19 +376,12 @@ cap = cv2.VideoCapture(0)
 
 
 
-
-
-
-
-
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
 parser.add_argument('--my_score', action='store_true')
 parser.add_argument('--Fahad_score', action='store_true')
 parser.add_argument('--Amrit_score', action='store_true')
 parser.add_argument('--score_with_images', action='store_true')
-#parser.add_argument('score', type=str, help='Score the pipeline' )
-#parser.add_argument('score with showing images', type=str, help='Scores the pipeline, shwoing all the images')
 args = parser.parse_args()
 
 
@@ -487,51 +400,24 @@ if args.my_score:
         img_count, scores, ratio,precision, recall = score(original_ids, predicted_ids)
         info(img_count, scores, ratio,precision, recall)
        
-        # Perform further processing on the image or store it as needed
+        
     print("Batch complete")
 
-# 1 -  Load the data - ✔ 
 
-    #image_dict = load_images(image_directory)
-
-
- #   original_ids = original_id(image_dict)
-
-# note: MY ppln  Uncomment once switxh to my ppln, and comment F_ppln 
-#predicted_ids = predicted_id2_c(image_dict)
-
-#F_ppln - ✔
-
- #   predicted_ids = My_predicted_id(image_dict)
-
-
-#    img_count,scores,ratio = score(original_ids,predicted_ids)
-
-#    print(' ')
-
-#    print('Printing My ppln score') 
-
-#   print(' ')
-
-#    info(img_count,scores,ratio)
-
+#F_ppln with args - ✔
 
 
 if args.Fahad_score:
    
-   # image_dict = load_images(image_directory)
-
-    #Check 41 img dataset 
-   #  directory = r'D:\AI research internship\opencv_scripts\data_set'
-    directory = r'D:\AI research internship\opencv_scripts\data_set'
+  
+    batch_size = 150
+  
+    directory = r'D:\AI research internship\opencv_scripts\n_updwn_angl'
     print('Fahd ppln score: ')   
     print('')
-    #original_ids = original_id(image_dict)
+    
 
-# note: MY ppln  Uncomment once switxh to my ppln, and comment F_ppln 
-#predicted_ids = predicted_id2_c(image_dict)
 
-#F_ppln - ✔
     for batch in load_images_in_batches(directory, batch_size):
         original_ids = original_id(batch)
         predicted_ids = F_predicted_id2(batch)
@@ -541,15 +427,16 @@ if args.Fahad_score:
 
 if args.Amrit_score:
    
-   # image_dict = load_images(image_directory)
-   
     print('Amrit ppln score: ')  
     print('')
 
     batch_size = 150
 
    # Check 41 img dataset 
-    directory = r'D:\AI research internship\opencv_scripts\data_set'
+
+   # Check 2K img dataset 
+
+    directory = r'D:\AI research internship\opencv_scripts\n_updwn_angl'
 
     def A_predicted_id(image_dict):
         p_id_arr = []
@@ -581,37 +468,17 @@ if args.Amrit_score:
         img_count, scores, ratio,precision, recall = score(new_arr_predicted_ids,original_ids)
         info(img_count, scores, ratio,precision, recall)
        
-        # Perform further processing on the image or store it as needed
+        
     print("Batch complete")
-
-
-
-# note: MY ppln  Uncomment once switxh to my ppln, and comment F_ppln 
-#predicted_ids = predicted_id2_c(image_dict)
-
-#F_ppln - ✔
-''''
-
-    predicted_ids = F_predicted_id2(image_dict)
-
-
-    img_count,scores,ratio = score_c(original_ids,predicted_ids)
-
-    info(img_count,scores,ratio)
-    '''
    
 
 if args.score_with_images:
- 
-   #image_dict = load_images(image_directory)
 
 
    original_ids = original_id(image_dict)
 
 
    predicted_ids = predicted_id(image_dict)
-
- #Code Check predicted_ids - works 
 
    print(predicted_ids)
 
@@ -620,7 +487,6 @@ if args.score_with_images:
    info(img_count,scores,ratio)
  
    while(True):
-    #  ret, frame = cap.read()
      for key, values in image_dict.items():
          img1 = cv2.imread(key)
          img2 = processed(img1)
@@ -656,30 +522,6 @@ def score(original_id, predicted_id):
     ratio = (scores / total) * 100
     print('Scores:', scores)
     return scores, total, ratio, precision, recall
-
-original = [1, 1, 1,
-        1, 22, 22, 22, 22, 22]
-
-
-pred = [1, 1, None, 1, 22, 22, None, None, 22]
-
-scores, total, ratio, precision, recall = score(pred, original)
-print(info(scores, total, ratio, precision, recall))
-
-# Testing the scoring fucntion  
-
-
-#A1 = [1,2,3,44,22,33]
-#A2 = [1,None, 3, None, None,33]
-
-
-
-
-#Test worked, converted array of array to an int, flipped orig, to predict. 
-
-
-
-#AA = A_predicted_id2(image_dict)
 
 
 
